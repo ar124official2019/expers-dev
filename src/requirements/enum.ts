d9e4035aca8b7088df30053e208be18eeddf7be1
Ahmad Raza <pro.se.ahmad.raza.1@gmail.com>
2022-01-11 18:13:24 +0500

    fix enum validator

--- src/requirements/enum.ts
+++ src/requirements/enum.ts
@@ -7,24 +7,10 @@ import {
 import { getError } from "./get-error";
 
 export function enums(value: any, requirement: IRequirement) {
-  let val, error;
+  let error;
 
   if (requirement.dataType == DataType.array) {
-    if (
-      requirement.type == RequirementType.queryParam ||
-      requirement.type == RequirementType.header
-    ) {
-      try {
-        val = JSON.parse(value);
-      } catch {
-        error = true;
-      }
-    } else if (Array.isArray(value)) {
-      val = value;
-    }
-
-    if (!error) error = !Array.isArray(val);
-
+    error = !requirement.enum?.includes(value);
     if (error) {
       return getError(
         requirement.type,
